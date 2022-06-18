c70a85537aa98cd48961729dfe580e8cc31884ca
emmanuelojiji <emmanuelojiji@hotmail.com>
2022-06-18 02:51:34 +0100

    User Info Context added

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Editor from "./Pages/Editor";
+import { UserInfoProvider } from "./Contexts/UserInfoContext";
 
 function App() {
-
- 
-
-
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/editor" element={<Editor />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <UserInfoProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/editor" element={<Editor />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </UserInfoProvider>
     </div>
   );
 }
--- src/Pages/Editor.js
+++ src/Pages/Editor.js
@@ -6,71 +6,59 @@ import { useState } from "react";
 import MinimalistTemplate from "../Components/MinimalistTemplate";
 import Designer from "../Components/Designer";
 
-const Editor = () => {
-  const [editorView, setEditorView] = useState("personal");
+import UserInfoContext from "../Contexts/UserInfoContext";
+import { useContext } from "react";
+import Sidebar from "../Components/Sidebar";
 
-  const [templatesVisible, setTemplatesVisible] = useState(false);
-  const [notificationVisibile, setNotificationVisibile] = useState(false);
-  const [designerVisible, setDesignerVisible] = useState(false);
+const Editor = () => {
+  const {
+    name,
+    setName,
+    profile,
+    setProfile,
+    jobTitle,
+    setJobTitle,
+    address,
+    setAddress,
+    phoneNumber,
+    setPhoneNumber,
+    email,
+    setEmail,
+    experienceJobTitle,
+    setExperienceJobTitle,
+    experienceCompany,
+    setExperienceCompany,
+    experienceStartYear,
+    setExperienceStartYear,
+    experienceEndYear,
+    setExperienceEndYear,
+    experienceJobDescription,
+    setExperienceJobDescription,
+    schoolName,
+    setSchoolName,
+    educationStartYear,
+    setEducationStartYear,
+    educationEndYear,
+    setEducationEndYear,
+    experienceArray,
+    setExperienceArray,
+    educationArray,
+    setEducationArray,
+  } = useContext(UserInfoContext);
 
   const [selectedTemplate, setSelectedTemplate] = useState("minimalist");
-
-  const [name, setName] = useState();
-  const [profile, setProfile] = useState();
-  const [jobTitle, setJobTitle] = useState();
-  const [address, setAddress] = useState();
-  const [phoneNumber, setPhoneNumber] = useState();
-  const [email, setEmail] = useState();
+  const [templatesVisible, setTemplatesVisible] = useState(false);
+  const [notificationVisible, setNotificationVisible] = useState(false);
+  const [designerVisible, setDesignerVisible] = useState(false);
 
   const [addingExperience, setAddingExperience] = useState(false);
-
-  const [experienceArray, setExperienceArray] = useState([]);
-  const [experienceJobTitle, setExperienceJobTitle] = useState();
-  const [experienceCompany, setExperienceCompany] = useState();
-  const [experienceStartYear, setExperienceStartYear] = useState();
-  const [experienceEndYear, setExperienceEndYear] = useState();
-  const [experienceJobDescription, setExperienceJobDescription] = useState();
-  const [currently, setCurrently] = useState(false);
-
   const [addingEducation, setAddingEducation] = useState(false);
-  const [educationArray, setEducationArray] = useState([]);
-  const [schoolName, setSchoolName] = useState();
-  const [educationStartYear, setEducationStartYear] = useState();
-  const [educationEndYear, setEducationEndYear] = useState();
-
-  const clearExperienceFields = () => {
-    setExperienceJobTitle();
-    setExperienceCompany();
-    setExperienceStartYear();
-    setExperienceEndYear();
-    setExperienceJobDescription();
-  };
-
-  const clearEducationFields = () => {
-    setSchoolName("");
-    setEducationStartYear("");
-    setEducationEndYear("");
-  };
-
-  const experienceObject = {
-    experienceJobTitle: experienceJobTitle,
-    experienceCompany: experienceCompany,
-    experienceStartYear: experienceStartYear,
-    experienceEndYear: experienceEndYear,
-    experienceJobDescription: experienceJobDescription,
-  };
-
-  const educationObject = {
-    schoolName: schoolName,
-    educationStartYear: educationStartYear,
-    educationEndYear: educationEndYear,
-  };
 
   return (
     <div className="Editor">
       {designerVisible && <Designer />}
 
-      {notificationVisibile && (
+      {notificationVisible && (
         <div className="notification">
           Make sure you're including your best work only!
         </div>
@@ -129,255 +117,13 @@ const Editor = () => {
         </div>
       </div>
 
-      <div className="controls">
-        <Link to="/" className="back">
-          Back
-        </Link>
-        <div class="controls-nav">
-          <h4
-            style={{ color: editorView === "personal" && "#1b91f0" }}
-            onClick={() => setEditorView("personal")}
-          >
-            Personal
-          </h4>
-          <h4
-            style={{ color: editorView === "experience" && "#1b91f0" }}
-            onClick={() => setEditorView("experience")}
-          >
-            Experience
-          </h4>
-          <h4
-            style={{ color: editorView === "education" && "#1b91f0" }}
-            onClick={() => setEditorView("education")}
-          >
-            Education
-          </h4>
-          <h4
-            style={{ color: editorView === "skills" && "#1b91f0" }}
-            onClick={() => setEditorView("skills")}
-          >
-            Skills
-          </h4>
-        </div>
-
-        {(() => {
-          if (editorView === "personal") {
-            return (
-              <div className="personal-view">
-                <div className="input-wrap">
-                  <input
-                    type="text"
-                    placeholder="Full Name"
-                    onChange={(e) => setName(e.target.value)}
-                    value={name}
-                  ></input>
-                </div>
-
-                <div className="input-wrap">
-                  <input
-                    type="text"
-                    placeholder="Job Title"
-                    onChange={(e) => setJobTitle(e.target.value)}
-                    value={jobTitle}
-                  ></input>
-                </div>
-
-                <div className="input-wrap">
-                  <input
-                    type="text"
-                    placeholder="Address"
-                    onChange={(e) => setAddress(e.target.value)}
-                    value={address}
-                  ></input>
-                </div>
-
-                <div className="input-wrap">
-                  <input
-                    type="number"
-                    placeholder="Phone Number"
-                    onChange={(e) => setPhoneNumber(e.target.value)}
-                    value={phoneNumber}
-                  ></input>
-                </div>
-
-                <div className="input-wrap">
-                  <input
-                    type="text"
-                    placeholder="Email Address"
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                  ></input>
-                </div>
-
-                <div className="input-wrap">
-                  <textarea
-                    type="text"
-                    placeholder="Profile"
-                    onChange={(e) => setProfile(e.target.value)}
-                    value={profile}
-                  ></textarea>
-                </div>
-              </div>
-            );
-          } else if (editorView === "experience") {
-            return (
-              <>
-                {addingExperience === false && (
-                  <button
-                    onClick={() => setAddingExperience(true)}
-                    className="button add-button"
-                  >
-                    Add Experience
-                  </button>
-                )}
-
-                {addingExperience && (
-                  <>
-                    <h4 className="experience-title">
-                      Work Experience {experienceArray.length + 1}
-                    </h4>
-                    <input
-                      type="text"
-                      placeholder="Job Title"
-                      onChange={(e) => setExperienceJobTitle(e.target.value)}
-                      value={experienceJobTitle}
-                    ></input>
-                    <input
-                      type="text"
-                      placeholder="Company"
-                      onChange={(e) => setExperienceCompany(e.target.value)}
-                      value={experienceCompany}
-                      required
-                    ></input>
-                    <input
-                      type="text"
-                      placeholder="Start Year"
-                      onChange={(e) => setExperienceStartYear(e.target.value)}
-                      value={experienceStartYear}
-                    ></input>
-                    <input
-                      type="text"
-                      placeholder="End Year"
-                      onChange={(e) => setExperienceEndYear(e.target.value)}
-                      value={experienceEndYear}
-                    ></input>
-                    <div className="checkbox-wrap">
-                      <input
-                        type="checkbox"
-                        onChange={() => {
-                          if (currently === false) {
-                            setCurrently(true);
-                            setExperienceEndYear("Present");
-                          } else {
-                            setCurrently(false);
-                            setExperienceEndYear("");
-                          }
-                        }}
-                      ></input>
-                      <span>I still work here</span>
-                    </div>
-                    <textarea
-                      placeholder="Job Description"
-                      onChange={(e) =>
-                        setExperienceJobDescription(e.target.value)
-                      }
-                      value={experienceJobDescription}
-                    ></textarea>
-                    <button
-                      className="button save"
-                      onClick={() => {
-                        setExperienceArray([
-                          experienceObject,
-                          ...experienceArray,
-                        ]);
-                        clearExperienceFields();
-                        console.log(experienceArray);
-                        setAddingExperience(false);
-                        setCurrently(false);
-
-                        if (experienceArray.length == 3) {
-                          setNotificationVisibile(true);
-                        }
-                      }}
-                    >
-                      Save
-                    </button>
-                  </>
-                )}
-
-                {experienceArray.map((experience) => {
-                  return (
-                    <div class="experience-tab">
-                      {experience.experienceJobTitle}
-                      <i class="fa-solid fa-ellipsis"></i>
-                    </div>
-                  );
-                })}
-              </>
-            );
-          } else if (editorView === "education") {
-            return (
-              <>
-                {addingEducation ? (
-                  <>
-                    <input
-                      type="text"
-                      placeholder="School Name"
-                      onChange={(e) => setSchoolName(e.target.value)}
-                      value={schoolName}
-                    ></input>
-                    <input
-                      type="text"
-                      placeholder="Start Year"
-                      onChange={(e) => setEducationStartYear(e.target.value)}
-                      value={educationStartYear}
-                    ></input>
-                    <input
-                      type="text"
-                      placeholder="End Year"
-                      onChange={(e) => setEducationEndYear(e.target.value)}
-                      value={educationEndYear}
-                    ></input>
-                    <button
-                      className="button save"
-                      onClick={() => {
-                        setEducationArray([educationObject, ...educationArray]);
-                        clearEducationFields();
-                        setAddingEducation(false);
-                        setCurrently(false);
-
-                        if (educationArray.length == 3) {
-                          setNotificationVisibile(true);
-                        }
-                      }}
-                    >
-                      Save
-                    </button>
-                  </>
-                ) : (
-                  <button
-                    onClick={() => setAddingEducation(true)}
-                    className="button add-button"
-                  >
-                    Add Education
-                  </button>
-                )}
-
-                {educationArray.map((education) => {
-                  return (
-                    <div class="experience-tab">
-                      {education.schoolName}
-                      <i class="fa-solid fa-ellipsis"></i>
-                    </div>
-                  );
-                })}
-              </>
-            );
-          } else if (editorView === "skills") {
-            return <p>Skills</p>;
-          }
-        })()}
-      </div>
+      <Sidebar
+        setNotificationVisibile={setNotificationVisible}
+        addingExperience={addingExperience}
+        setAddingExperience={setAddingExperience}
+        addingEducation={addingEducation}
+        setAddingEducation={setAddingEducation}
+      />
 
       <div className="preview">
         <div
